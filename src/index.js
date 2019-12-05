959cd2c6f8a568ec48c0d44cb660953d77ddc537
wxx <1079565530@qq.con>
2019-12-05 11:02:15 +0800

    init app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+function Button(){
+    return (<div>hhhhh</div>)
+}
+
+
+ReactDOM.render(<Button/>, document.getElementById('app'))
